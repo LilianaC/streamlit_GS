bad582c0441796aa8d2e56d04658620592d8ebfa
LilianaC <liliana.castanon@gmail.com>
2024-04-19 17:18:50 -0600

    Create Codeavra.py

---
+++ Codeavra.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from google.oauth2 import service_account
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "americas"
+spread = Spread(spreadsheetname,client = client)
+
+# Check the connection
+st.write(spread.url)
+
+sh = client.open(spreadsheetname)
+worksheet_list = sh.worksheets()
